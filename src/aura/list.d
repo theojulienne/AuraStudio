af7036b92e88cc40b9f584671af05c79580317c3
Theo Julienne <theo.julienne@gmail.com>
2007-06-03 11:34:34 +0000

    Dunce cap

---
+++ src/aura/list.d
@@ -0,0 +1,69 @@
+module aura.list;
+
+class List(T)
+{
+	T[] l;
+	
+	void append( T i )
+	{
+		int n = l.length;
+		l.length = n+1;
+		l[n] = i;
+	}
+	
+	int find( T i )
+	{
+		foreach ( a, ti; l )
+		{
+			if ( ti == i )
+				return a;
+		}
+		
+		return -1;
+	}
+	
+	void remove( T i )
+	{
+		foreach ( a, ti; l )
+		{
+			if ( ti == i )
+			{
+				auto n = l[0..a];
+				auto m = l[a+1..length];
+				l = n ~ m;
+				return;
+			}
+		}
+	}
+	
+	T opIndex( int a )
+	{
+		return l[a];
+	}
+	
+	T[] opAssign( T[] il )
+	{
+		l = il;
+		
+		return l;
+	}
+	
+	T opIndexAssign( T v, int a )
+	{
+		l[a] = v;
+		
+		return l[a];
+	}
+	
+	int opApply( int delegate(inout T) dg )
+	{
+		foreach ( i; l )
+			dg( i );
+		return 0;
+	}
+	
+	T[] get( )
+	{
+		return l;
+	}
+}

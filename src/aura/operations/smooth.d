ad6a60f1526e65c37840936c0da9e39135f48d77
David Collien <davidcollien@gmail.com>
2007-06-11 11:28:30 +0000

    oops, added smooth.d

---
+++ src/aura/operations/smooth.d
@@ -0,0 +1,175 @@
+module aura.operations.smooth;
+
+import std.stdio;
+
+import aura.model.all;
+import aura.operation;
+import aura.list;
+
+class SmoothOperation : Operation
+{
+	
+	VertexList orig_verts;
+	VertexList edge_verts;
+	VertexList face_verts;
+	Body b;
+	
+	Vertex[Edge] vemap;
+	Vertex[Face] vfmap;
+	
+	this( )
+	{
+		orig_verts = new VertexList;
+		edge_verts = new VertexList;
+		face_verts = new VertexList;
+	}
+	
+	Vertex add_face_vert( Face f )
+	{
+		Vertex v = new Vertex( b, 0, 0, 0 );
+		
+		foreach ( fv; f.verts )
+		{
+			v += fv;
+		}
+		
+		v /= f.verts.length;
+		
+		face_verts.append( v );
+		
+		return v;
+	}
+	
+	Vertex add_edge_vert( Edge e )
+	{
+		Vertex v = Vertex.makeCenterOf( e.va, e.vb );
+		Vertex vb = new Vertex( b, 0, 0, 0 );
+		vb += v;
+		
+		edge_verts.append( vb );
+		vemap[e] = vb;
+		
+		return vb;
+	}
+	
+	Vertex get_vert_for_edge( Edge e )
+	{
+		if ( !( e in vemap ) )
+			return add_edge_vert( e );
+		
+		return vemap[e];
+	}
+	
+	bool prepare( Selection sel )
+	{
+		if ( !Operation.prepare( sel ) )
+			return false;
+		
+		auto faces = sel.getFaces( );
+		sel.resetSelection( );
+		
+		VertexList ops = new VertexList;
+		
+		b = faces[0].f_body;
+		
+		foreach ( n, f; faces )
+		{
+			Vertex vc = add_face_vert( f );
+			
+			vfmap[f] = vc;
+			
+			VertexList vl = new VertexList;
+			
+			for ( int en = 0; en < f.edges.length; en++ )
+			{
+				int et = en-1;
+				
+				if ( et < 0 )
+					et = f.edges.length-1;
+				
+				Vertex vt = get_vert_for_edge( f.edges[en] );
+				Vertex vp = get_vert_for_edge( f.edges[et] );
+				
+				Vertex vo = f.edges[en].getCommonVertex( f.edges[et] );
+				
+				b.addFace( 4, vc, vp, vo, vt );
+				
+				ops.appendUnique( vo );
+			}
+			
+			f.f_body.removeFace( f );
+		}
+		
+		foreach ( P; ops )
+		{
+			Vertex F = new Vertex( null, 0, 0, 0 );
+			
+			int a = 0;
+			
+			writefln( "P.faces.length: %d", P.faces.length);
+			writefln( "vfmap.length: %d", vfmap.length);
+			foreach ( vf; P.faces )
+			{	
+				if ( !( vf in vfmap ) )
+					continue;
+				
+				// never gets here
+				
+				F += vfmap[vf];
+
+				writefln( "F-XYZ: %f %f %f", F.x, F.y, F.z );
+				
+				
+				a++;
+			}
+			
+			F /= a;
+
+
+			Vertex R = new Vertex( null, 0, 0, 0 );
+			a = 0;
+			
+			foreach ( ve; P.edges )
+			{
+				if ( !( ve in vemap ) )
+					continue;
+				
+				R += vemap[ve];
+
+				
+				writefln( "R-XYZ: %f %f %f", R.x, R.y, R.z );
+				
+				
+				a++;
+			}
+			
+			
+			R /= a;
+			
+			
+			Vertex PC = new Vertex( P );
+			PC = (F + (R*2) + ((a-3) * P)) / a;
+			
+			writefln( "XYZ: %f %f %f", PC.x, PC.y, PC.z );
+			P.zero;
+			P += PC;
+			
+		}
+		
+		return true;
+	}
+	
+	void update( )
+	{
+	}
+	
+	void complete( )
+	{
+		
+	}
+	
+	void cleanup( )
+	{
+		Operation.cleanup( );
+	}
+}
